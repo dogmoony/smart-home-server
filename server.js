96743b4ae53f77e03cb746ecbdb4b665d7f0d38d
dogmoony <k.kushnir.tv12@kpi.ua>
2024-10-29 17:42:07 +0000

    Changed the message for website

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ let notifications = [
 
 // Sample route
 app.get("/", (req, res) => {
-  res.send("Smart Home Assistant API with WEB!");
+  res.send("Smart Home Assistant API with WEB work please!");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
