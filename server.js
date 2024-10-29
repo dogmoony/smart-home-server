a8f66d146737315e4eab1abdbbe1f3260bd49941
dogmoony <k.kushnir.tv12@kpi.ua>
2024-10-29 23:01:46 +0000

    Added login page

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static("public"));
-app.use(cors());
-app.use(bodyParser.json());
+// Middleware to parse JSON data from the request body
+app.use(express.json());
+app.use(express.static("./client/public")); // Serve static files from 'public' folder
 
 let devices = [
   { id: 1, name: "Smart Light", type: "light", status: "off" },
