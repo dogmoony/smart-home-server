0a25a225aefa5d6e335893e7aa7e457fed06e7ee
dogmoony <k.kushnir.tv12@kpi.ua>
2024-10-30 11:50:22 +0000

    Added database connection to the server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const db = require("./db");
 const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON data from the request body
