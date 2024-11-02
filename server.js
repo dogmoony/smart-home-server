d2e336aea0d1b26e12a2ac2b34335df45ae7cdf8
dogmoony <k.kushnir.tv12@kpi.ua>
2024-11-02 20:07:16 +0000

    Trying to add database connection

--- server.js
+++ server.js
@@ -67,9 +67,10 @@ app.post("/auth/login", async (req, res) => {
     const { email, password } = req.body;
 
     // Query the database to find the user
+    const trimmedEmail = email.trim();
     const result = await pool.query(
       "SELECT * FROM users WHERE email ILIKE $1",
-      [email]
+      [trimmedEmail]
     );
 
     console.log("Query result:", result.rows); // Log the result of the query
