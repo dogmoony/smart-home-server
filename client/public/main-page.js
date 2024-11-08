1b68145d5e20e71b8ccb6734d76393bd30a95f98
dogmoony <k.kushnir.tv12@kpi.ua>
2024-11-08 22:59:01 +0000

    Making main page

--- client/public/main-page.js
+++ client/public/main-page.js
@@ -161,3 +161,15 @@ window.addEventListener("DOMContentLoaded", () => {
     fetchDevices(true); // Fetch devices after confirming the user is authenticated
   }
 });
+
+function showPage(pageId) {
+  // Hide all pages
+  const pages = document.querySelectorAll(".page");
+  pages.forEach((page) => {
+    page.classList.remove("active");
+  });
+
+  // Show the selected page
+  const selectedPage = document.getElementById(pageId);
+  selectedPage.classList.add("active");
+}
